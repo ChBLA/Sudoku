9ba233d90fc5df876b7816104fa6da0b0284ee1d
ChBLA <cbla19@student.aau.dk>
2021-07-06 20:44:00 +0200

    Solver includes time handling

--- Sudoku/BaseGame/classes/Solver.cs
+++ Sudoku/BaseGame/classes/Solver.cs
@@ -7,9 +7,22 @@ namespace Sudoku.BaseGame.classes
 {
     class Solver
     {
+        private int reqCounter = -1;
+        private DateTime startTime;
+        private const int timeoutInSec = 30;
 
-        public Board solveSuduko(Board initial)
+        public Board solveSudoku(Board initial, int depth)
         {
+            this.startTime = DateTime.Now;
+            return solve(initial, depth);
+        }
+
+        private Board solve(Board initial, int depth)
+        {
+            reqCounter++;
+            if ((DateTime.Now - startTime).TotalSeconds > timeoutInSec)
+                throw new TimeoutException();
+            //initial.printBoard();
             if (this.isInvalid(initial))
                 return null;
             if (this.isSolved(initial))
@@ -23,33 +36,30 @@ namespace Sudoku.BaseGame.classes
                     foreach (int move in moves)
                     {
                         Board second = first.insertValue(c, move);
-                        Board solution = solveSuduko(second);
+                        if (second == null)
+                            break;
+                        Board solution = solve(second, depth + 1);
                         if (solution != null)
                             return solution;
                     }
                 }
             }
 
-            return null;
+            return isSolved(first) ? first : null;
         }
 
-        /*
-        for cell in B'
-        if not cell.isSolved() 
-            let S = cell.possibleValues()
-            for val in S 
-                let B'' = B'.makeMove(cell, val)
-                let Solution = solveSuduko(B'')
-                if (Solution != null)
-                    return Solution
-        return null*/
+        public int getReqCounter()
+        {
+            return this.reqCounter;
+        }
 
         private Board makeDeterministicMoves(Board initial)
         {
-            bool hasBeenChanged = false;
-            Board answer = initial;
+            Board answer = initial.duplicate();
+            bool hasBeenChanged;
             do
             {
+                hasBeenChanged = false;
                 foreach (Cell cell in answer.getCells())
                 {
                     List<int> moves = answer.possibleValues(cell);
@@ -65,12 +75,54 @@ namespace Sudoku.BaseGame.classes
 
         private bool isSolved(Board initial)
         {
+            if (checkGrid(initial.getRows()) && checkGrid(initial.getColumns()) && checkGrid(initial.getSquares()))
+                return true;
+            return false;
+        }
+
+        private bool checkGrid(List<List<Cell>> grid)
+        {
+            foreach (List<Cell> row in grid)
+            {
+                var checker = makeCheckArray();
+                foreach (Cell cell in row)
+                {
+                    if (cell.getValue() != Cell.EmptyVal)
+                    {
+                        checker[cell.getValue() - 1] = true;
+                    }
+                }
+
+                if (!checkChecker(checker))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool checkChecker(bool[] checker)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (!checker[i])
+                    return false;
+            }
             return true;
         }
 
         private bool isInvalid(Board initial)
         {
             return !initial.isValid();
+        }
+
+        private bool[] makeCheckArray()
+        {
+            bool[] answer = new bool[9];
+            for (int i = 0; i < 9; i++)
+            {
+                answer[i] = false;
+            }
+            return answer;
         }
     }
 }
