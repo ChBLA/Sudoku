55cc044e5c44599cf57f2e6d4849ec6468de660c
ChBLA <cbla19@student.aau.dk>
2021-07-06 20:42:53 +0200

    Cell now includes row and column
    Used to more easily access the row, column, and square it is part of

--- Sudoku/BaseGame/classes/Cell.cs
+++ Sudoku/BaseGame/classes/Cell.cs
@@ -7,12 +7,15 @@ namespace Sudoku.BaseGame.classes
     class Cell
     {
         private int value;
-
+        private int row;
+        private int column;
         public static int EmptyVal = 0;
 
-        public Cell(int value)
+        public Cell(int value, int row, int column)
         {
             this.value = value;
+            this.row = row;
+            this.column = column;
         }
 
         public int getValue()
@@ -25,9 +28,28 @@ namespace Sudoku.BaseGame.classes
             return this.value != EmptyVal;
         }
 
-         public enum Value
+        public override bool Equals(object obj)
+        {
+            return obj is Cell cell &&
+                   value == cell.value &&
+                   row == cell.row &&
+                   column == cell.column;
+        }
+
+        public enum Value
         {
             EmptyVal = 0
         }
+
+        public int getRow()
+        {
+            return this.row;
+        }
+
+        public int getColumn()
+        {
+            return this.column;
+        }
+        
     }
 }
